7885d176e7e6127770d794828ed8487a0986dc89
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-12-08 19:58:41 -0600

    Added Home page

--- config/routes.rb
+++ config/routes.rb
@@ -106,6 +106,11 @@ FinalProjectV7::Application.routes.draw do
   delete '/ideas/:id', controller: 'ideas', action: 'destroy'
   #------------------------------
 
+  get  '/home', controller: 'pages', action: 'home'
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
